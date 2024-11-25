5f1946603a2b7e6dae7ac2d2f7469122d550583f
Jaekyun Kim <jkim681@myseneca.ca>
2024-11-24 23:22:22 -0500

    Made changes based on testing on linux vm

--- user_functions/report_of_user.py
+++ user_functions/report_of_user.py
@@ -15,9 +15,11 @@ def get_regular_users():
     for user in all_users:
         # Since getpwall fetchese all users, including system users. we need to filter out these.
         # After doing some research, modern linux distros generally uses uid greater th1an 1000 for regular users
-        if user.pw_uid >= 1000:
+        # root pw_uid is 0 so we should capture this as well
+	    # in certain distro nobody user has uid greater then 1000
+        if (user.pw_uid >= 1000 or user.pw_uid == 0) and user.pw_name != "nobody":
             # we only want the username (this may change)
-            regular_users.append(user.pw_name)
+            regular_users.append((user.pw_name,user.pw_dir))
     
     return regular_users
 
@@ -30,30 +32,31 @@ def get_last_login(user):
     last_login_command = os.popen(f"last {user}")
     # Since it is file object we need to read the file
     last_login_data = last_login_command.read()
+    # when there is time i should try to clean the data
     return last_login_data
 
 def is_sudoer(user):
     """
     This function will return True if the user is a sudoer, False otherwise.
     """
-    # The sudoers file is located in /etc/sudoers
-    sudoers_file = open("/etc/sudoers", "r")
-    # Read the file
-    sudoers_data = sudoers_file.read()
-    # Check if the user is in the sudoers file
-    sudoers_file.close()
     # may need to check user group as well if the user is in sudo group
     # may need to add which folder the user has sudo access to
-    if user in sudoers_data:
-        return "User is a sudoer."
-    return "User is not a sudoer."
+    # this problem is solved by running sudo -l -U command
+    user_privileges = os.popen(f"sudo -l -U {user}").read()
 
-def get_user_disk_usage(user):
+    # if have time should try to clean the data
+    return user_privileges
+
+def get_user_disk_usage(user_info):
     """
     This function will return the disk usage of the user.
     """
     # This will run the command and return the output as file object
-    disk_usage_command = os.popen(f"quota -u {user}")
+    try:
+        disk_usage_command = os.popen(f"du -sh {user_info[1]}")
+    except:
+        return f"Unable to get disk usage for the user:{user_info[0]} This could be because user is system user"
+    
     # Read the file
     disk_usage_data = disk_usage_command.read()
     return disk_usage_data
@@ -78,16 +81,16 @@ def user_report():
     # need to use the list of users to get information about regualr users
     for user in users:
         report_file.write("-------------------------\n")
-        report_file.write(f"User: {user}\n")
+        report_file.write(f"User: {user[0]}\n")
+        # need to check if the user is sudoer or not
+        report_file.write(f"Permissions: \n")
+        report_file.write(is_sudoer(user[0]))
         # need to check disk usage
         report_file.write(f"Disk Usage: \n")
         report_file.write(get_user_disk_usage(user))
         # need to check last login 
         report_file.write(f"Last Login: \n")            
-        report_file.write(get_last_login(user))
-        # need to check if the user is sudoer or not
-        report_file.write(f"Permissions: \n")
-        report_file.write(is_sudoer(user))
+        report_file.write(get_last_login(user[0]))
         report_file.write("\n")
     
     report_file.close()
