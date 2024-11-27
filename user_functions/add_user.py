7f44fb6e6fa77d2edb299832a89c27b3f78e6c55
ldiminutto <lawson.diminutto12@gmail.com>
2024-11-27 12:54:26 -0500

    add user

--- user_functions/add_user.py
+++ user_functions/add_user.py
@@ -1,7 +1,15 @@
 #!usr/bin/env/python3
 
+import subprocess
+
 def add_user():
     """
     This function will add a user to the system.
     """
     pass
+
+# Get the username from the user
+    username = input("Enter the username for the new user: ").strip()
+    if not username:
+        print("Error: Username cannot be empty.")
+        return
